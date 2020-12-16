13747a1c2987f617bf73a0702e7ccac7ab0d93e8
feodor <feodor.kuyanov@gmail.com>
2020-12-16 20:06:49 +0300

    Updated JDK config

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,13 +1,9 @@
-import kotlinx.browser.document
 import react.*
-import react.dom.p
-
-val fieldSizes = listOf(6, 7, 8)
 
 class App : RComponent<RProps, RState>() {
     override fun RBuilder.render() {
         child(FieldList::class) {
-            attrs.fieldSizes = fieldSizes
+            attrs.fieldSizes = listOf(6, 7, 8)
         }
     }
 }
